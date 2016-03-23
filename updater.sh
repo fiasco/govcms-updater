45d1b6aa314b2e242f61fc16a0bf4688279f4b10
Josh Waihi <josh.wahi@acquia.com>
2016-03-23 23:22:38 +1300

    Validated composer has run before enabling tool

--- updater.sh
+++ updater.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
 updater_dir=`dirname $0`;
 
 opts='';
 if [ "$1" != "" ]; then
   opts="-Dbuild.git.ref=$1";
+fi
+
+if [ ! -d ${updater_dir}/vendor ]; then
+  echo "Error: Composer has not installed dependancies. Please run: 'composer install' in ${updater_dir} first."
+  exit 2;
 fi
 
 ${updater_dir}/vendor/bin/phing -f ${updater_dir}/build.xml project:build $opts
